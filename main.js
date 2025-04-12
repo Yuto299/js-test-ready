889e66dd7013d5b88bc6b7571167eff4aba429d8
Yuto299 <ota.se.ke@outlook.jp>
2025-04-12 23:26:36 +0900

    while文

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
+//ループ処理
 // const arry = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
 
 // for (const elem of arry) {
 //   console.log(elem);
 // }
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let sum = 0;
+//ループ処理と合計値の計算
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let sum = 0;
 
-for (const num of numbers) {
-  console.log(num);
-  sum += num;
+// for (const num of numbers) {
+//   console.log(num);
+//   sum += num;
+// }
+// console.log(sum);
+
+// while文
+let i = 0;
+while (i <= 3) {
+  console.log(i);
+  i++; //これがないと無限ループになる
 }
-console.log(sum);
