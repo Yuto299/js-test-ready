c75f384f535d3769b8417c3b03feda419437ad5f
Yuto299 <ota.se.ke@outlook.jp>
2025-04-13 01:32:03 +0900

    追加、置換、削除

--- main.js
+++ main.js
@@ -33,9 +33,25 @@
 // arr.push(6);
 // console.log(arr); // [1, 2, 3, 4, 5, 6]
 
-// 末尾の要素を削除
-const arr = [1, 2, 3, 4, 5];
-const popped = arr.pop();
+// // 末尾の要素を削除
+// const arr = [1, 2, 3, 4, 5];
+// const popped = arr.pop();
 
-console.log(arr);
-console.log(popped);
+// console.log(arr);
+// console.log(popped);
+
+// 2番目に要素を追加
+let fruits = ['apple', 'banana', 'orange'];
+
+fruits.splice(2, 0, 'peach');
+console.log(fruits); // ['apple', 'banana', 'peach', 'orange']
+
+// 先頭に置換
+let fruits2 = ['apple', 'banana', 'orange'];
+fruits2.splice(0, 1, 'peach');
+console.log(fruits2); // ['peach', 'banana', 'orange']
+
+//  先頭を削除
+let fruits3 = ['apple', 'banana', 'orange'];
+fruits3.splice(0, 1);
+console.log(fruits3); // ['banana', 'orange']
