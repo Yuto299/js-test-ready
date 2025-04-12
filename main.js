3d09ffef26b1b1ab0f9e03aabeafcaa44dae5fa2
Yuto299 <ota.se.ke@outlook.jp>
2025-04-12 23:37:07 +0900

    配列数を求める

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 // switch文
-const subject = 'Math';
+// const subject = 'Math';
 
-switch (subject) {
-  case 'English':
-    console.log('英語');
-    break;
-  case 'Math':
-    console.log('数学');
-    break;
-  case 'Science':
-    console.log('科学');
-    break;
-  default:
-    console.log('その他の科目');
-}
+// switch (subject) {
+//   case 'English':
+//     console.log('英語');
+//     break;
+//   case 'Math':
+//     console.log('数学');
+//     break;
+//   case 'Science':
+//     console.log('科学');
+//     break;
+//   default:
+//     console.log('その他の科目');
+// }
+
+//配列数を求める
+const num = 12345;
+const numLength = num.toString().length;
+console.log(numLength);
