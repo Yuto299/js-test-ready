4a0665f7ae9ac47d618c1c88f92bfccbc0e095a3
Yuto299 <ota.se.ke@outlook.jp>
2025-04-12 23:31:32 +0900

    switch文

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+// switch文
+const subject = 'Math';
 
+switch (subject) {
+  case 'English':
+    console.log('英語');
+    break;
+  case 'Math':
+    console.log('数学');
+    break;
+  case 'Science':
+    console.log('科学');
+    break;
+  default:
+    console.log('その他の科目');
+}
