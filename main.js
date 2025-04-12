6c7536219289fd71f27c4c55ad6a51b53122ad37
Yuto299 <ota.se.ke@outlook.jp>
2025-04-13 00:02:06 +0900

    末尾の要素を削除

--- main.js
+++ main.js
@@ -29,6 +29,13 @@
 // console.log(arry);
 
 // 末尾に要素を追加
+// const arr = [1, 2, 3, 4, 5];
+// arr.push(6);
+// console.log(arr); // [1, 2, 3, 4, 5, 6]
+
+// 末尾の要素を削除
 const arr = [1, 2, 3, 4, 5];
-arr.push(6);
-console.log(arr); // [1, 2, 3, 4, 5, 6]
+const popped = arr.pop();
+
+console.log(arr);
+console.log(popped);
