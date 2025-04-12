10d061e3f3d26b436be7181a67ebb75c1e42fce2
Yuto299 <ota.se.ke@outlook.jp>
2025-04-12 23:15:28 +0900

    ループした後に合計表示

---
+++ main.js
@@ -0,0 +1,14 @@
+// const arry = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+
+// for (const elem of arry) {
+//   console.log(elem);
+// }
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let sum = 0;
+
+for (const num of numbers) {
+  console.log(num);
+  sum += num;
+}
+console.log(sum);
